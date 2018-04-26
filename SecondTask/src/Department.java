f18cdea06d94f93cb8365275c29ef75f5a9c1478
Xireaker <xirari18@gmail.com>
2018-04-26 23:01:28 +0400

    Save vers

---
+++ SecondTask/src/Department.java
@@ -0,0 +1,14 @@
+public class Department {
+    String name;
+    Employee[] employees;
+    int size;
+
+    public Employee[] getEmployees() {
+        return employees;
+    }
+
+    public Employee[] getEmployees(String jobTitle){
+        return employees;
+    }
+
+}
---
+++ SecondTask/src/Employee.java
@@ -0,0 +1,38 @@
+public class Employee {
+    String firstName;
+    String secondName;
+    String jobTitle;
+    int salary;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
