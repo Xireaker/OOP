f9d8e01799cb379ec4e8cc951c39119da76efc5f
Volodin Vitaly <xirari18@gmail.com>
2018-04-28 01:55:01 +0400

    11111

--- SecondTask/src/Department.java
+++ SecondTask/src/Department.java
@@ -19,7 +19,22 @@ public class Department {
         size = employees.length;
     }
     public void addEmployees(Employee employees){
-
+        boolean entered = false;
+        for(int index = 0; index < this.employees.length; index++){
+            if (this.employees[index] == null) {
+                this.employees[index] = employees;
+                entered = true;
+                break;
+            }
+        }
+        if(entered == false){
+            Employee[] buffer = new Employee[this.size];
+            System.arraycopy(this.employees,0,buffer,0,this.employees.length);
+            this.size = this.size * 2;
+            this.employees = new Employee[this.size];
+            System.arraycopy(buffer,0,this.employees,0,buffer.length);
+            this.employees[buffer.length+1] = employees;
+        }
     }
     public Employee[] getEmployees() {
         return employees;
